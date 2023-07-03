5cbe60dc1e6a608f070ea94a999070b6a53191fa
youngjune-jo <whdydwns0604@gmail.com>
2023-07-03 20:24:26 +0900

    Tutorial2

---
+++ JAVA/Tutorial2/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	final static int INT_MAX = 2147483647;
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		double double_Type = 170.7;
+		String string_Type = "조용준";
+		
+		System.out.println(a);
+		System.out.println("a+b = " + (a+b));
+	}
+
+}
