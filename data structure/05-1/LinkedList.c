c57adc001187baec416289a8e23e65d900d38738
youngjune-jo <whdydwns0604@gmail.com>
2023-07-04 18:02:58 +0900

    중간과정

--- data structure/05-1/LinkedList.c
+++ data structure/05-1/LinkedList.c
@@ -12,12 +12,35 @@ Node *add_data(int data_num,char *data_name){
 }
 
 void ListInit(List *plist){
+    plist -> tail = (Node*)malloc(sizeof(Node));
+    plist->tail->next=NULL;
+
     plist -> cur = NULL;
     plist -> before = NULL;
-    plist -> tail = NULL;
-    int numOf = 0;
+    plist->numOf = 0;
 }
 
 void add_list(List *plist,Node *pNode){
+    pNode->next=plist->tail;
+    plist->tail->next=pNode;
+    plist->tail->data=pNode->data;
+    
+    plist->numOf++;
 
+}
+
+void showyourlist(List *plist){
+    plist->cur=plist->tail->next;
+
+    while(1){
+        if(plist->cur == NULL){
+            continue;
+        }
+        printf("%d %s\n",plist->cur->data->num,plist->cur->data->name);
+        plist->cur=plist->tail->next;
+
+        if(plist->cur==NULL){
+            break;
+        }
+    }
 }
