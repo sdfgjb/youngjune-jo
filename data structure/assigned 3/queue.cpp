2d8b9b27816db677e77c31117b3fb885413ada06
조용준 <whdydwns0604@gmail.com>
2023-10-29 21:05:18 +0900

    중간

--- data structure/assigned 3/queue.cpp
+++ data structure/assigned 3/queue.cpp
@@ -1,17 +1,23 @@
 #include <string>
+#include <iostream>
 #include "queue.h"
 
 template<class T>
 Qstack<T>::Qstack(int c){
     capactiy = c;
-    T array = new T[capactiy];
+    if (capactiy < 1)
+        throw "capacity must be > 0";
+    array = new T[capactiy];
     head = 0;
     tail = 0;
 }
 
 template<class T>
 bool Qstack<T>::IsEmpty() const{
-    if(tail == 0) return true;
+    if ((tail - head) == 0) {
+        return true;
+    }
+    else { return false; }
 
 }
 
@@ -23,59 +29,108 @@ void Qstack<T>::ChangeSize1D(T*&a, const int oldsize, const int newSize){
     copy(a, a+min(oldsize,newSize),new_a);
     delete [] a;
     a=new_a;
-    
 }
 
 template <class T>
 void Qstack<T>::Push(const T& item){
     if(head == 0){
         if(tail == capactiy){
-            ChangeSize1D(araay,capactiy,capactiy+1);
-            
+            ChangeSize1D(array,capactiy,2*capactiy);
+            capactiy *=2;
         }
-        T &new_araay = new T[capactiy];
-        copy(araay, araay + tail, new_araay);
-        araay[head]= item;
-        for(int i = 1; i =< tail; i++){
-            araay[i]=new_araay[i-1];
+        T *new_array = new T[capactiy];
+        copy(array+head, array + tail, new_array);
+        array[head] = item;
+        for(int i = head+1; i <= tail; i++){
+            array[i]=new_array[i-1];
         }
+        delete [] new_array;
         tail+=1;
-        capactiy+=1;
-        delete new_araay[];
     }else{
-        araay[--head] == item;
+        array[--head] = item;
+        copy(array+head, array + tail, array);
     }
-    
 }
 
 template <class T>
-void Qstack<T>::Put(const T& item){
-    if(tail == capactiy){
-        if(2*size > capactiy){
-            ChangeSize1D(araay,capactiy,2*capactiy);
+void Qstack<T>::Put(const T& item) {
+    if (tail == capactiy) {
+        if (2*(tail-head) > capactiy) {  
+            ChangeSize1D(array, capactiy, 2*capactiy);
             capactiy *= 2;
-        }else{
-            copy(araay+head,araay+tail,araay);
-            
+        } else {                    
+            copy(array+head,array+tail, array);
+            tail -= head;
+            head = 0;
         }
     }
-    size+=1;
+    array[tail++] = item;
 }
 
 template <class T>
-T& Qstack<T>::Pop() const{
-
+T& Qstack<T>::Pop() {
+    if (IsEmpty())
+        throw "Qstack is empty";
+    T& value = array[head++];
+    if (head >= capactiy / 2) {
+        copy(array + head, array + tail + 1, array);
+        tail -= head;
+        head = 0;
+        if (4 * (tail - head) <= capactiy) {
+            ChangeSize1D(array, capactiy, capactiy / 2);
+            capactiy /= 2;
+        }
+    }
+    return value;
 }
 template <class T>
-T& Qstack<T>::Pull() const{
-
+T& Qstack<T>::Pull() {
+    if (IsEmpty())
+        throw "Qstack is empty";
+    T& value = array[tail--];
+    if (head >= capactiy / 2) {
+        copy(array + head, array + tail, array);
+        tail -= head;
+        head = 0;
+        if (4 * (tail - head) <= capactiy) {
+            ChangeSize1D(array, capactiy, capactiy / 2);
+            capactiy /= 2;
+        }
+    }
+    return value;
 }
 
 template <class T>
 T& Qstack<T>::Peek(int index){
-
+    try{
+        if (index >= 0 && index < tail) {
+            return array[index];
+        }
+        else if (index < 0 && index >= head - tail) {
+            return array[tail + index];
+        }
+        else {
+            throw index;
+        }
+    }
+    catch (T index) {
+        cout << index << " can't index number." << endl;
+    }
 }
 
-
-template class Qstack<int>;    // a Queue for ints
-template class Qstack<float>;  // a Queue for floats
+template <class T>
+string Qstack<T>::toString()
+{
+    string rval = "capacity = ";
+    rval += to_string(capactiy);
+    rval += "\nsize = ";
+    rval += to_string(tail-head);
+    for (int i = 0; i <= capactiy; i++) {
+        rval += "\n" + to_string(array[i]); 
+        rval += (i == head ? "<-- head" : (i == tail) ? "<-- tail" : "");
+    }
+    rval += "\n";
+    return rval;
+}
+template class Qstack<int>;    
+template class Qstack<float>;  
--- data structure/assigned 3/queueTest.cpp
+++ data structure/assigned 3/queueTest.cpp
@@ -1,10 +1,19 @@
+#include <string>
 #include <iostream>
 #include "queue.h"
 using namespace std;
 
 int main()
 {
-    Qstack<int>();
+    Qstack<int> test;
     
+    for (int i = 1; i <=15; i++){
+        test.Put(i*i);
+    }
+    cout << test.toString() << endl;
 
+    for (int i = 1; i <= 10; i++) {
+        test.Pull();
+    }     
+    cout << test.toString() << endl;
 }
